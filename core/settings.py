d3b6405bebf3cba4c7cdea7a398cd3af6bdda7f1
mrprintopv <124579336+printopv007@users.noreply.github.com>
2023-10-14 22:14:52 +0530

    deployed

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d4wqlaf_&@!^x2-oljg4rocv8$k75ckn7_@#u9n%t9h7gxg-8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
